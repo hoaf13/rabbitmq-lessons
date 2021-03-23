85565a822b08bdbbb0cfff280d0978fb759fb900
hoaf13 <gam.nhosmiles2000@gmail.com>
2021-03-23 15:54:01 +0700

    publish/subcribe

---
+++ publish_subcribe/emit_log.py
@@ -0,0 +1,18 @@
+import pika 
+import time 
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+product = 100 
+
+
+while True:
+	channel.basic_publish(exchange='logs', routing_key='', body= str(product))
+	print('[x] send {}'.format(product))
+	product += 1
+	time.sleep(0.5)
+
+	
+connection.close()
